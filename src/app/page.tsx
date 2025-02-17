09a1bd90958cc991823c5e9678c2c596ede72b6c
gggaspen <gggaspen@gmail.com>
2025-02-17 02:24:45 -0300

    touchs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Home from "./pages/home/Home";
+"use client";
 
-export default function Page() {
+import Home from "./pages/home/Home";
+import React, { useEffect } from "react";
+import { postCounter } from "./services/counter.service";
+
+function Page() {
+  useEffect(() => {
+    postCounter();
+  }, []);
+
   return (
     <>
       <Home />
     </>
   );
 }
+
+export default Page;
---
+++ src/app/services/counter.service.ts
@@ -0,0 +1,35 @@
+const postCounter = async () => {
+  try {
+    const response = await fetch(
+      "https://titin-back-production.up.railway.app/touch",
+      {
+        method: "POST",
+      }
+    );
+    console.log(response);
+    if (!response.ok) {
+      console.error("Error en la solicitud", response.status);
+    }
+  } catch (error) {
+    console.error("Error en la solicitud", error);
+  }
+};
+
+const getCounter = async () => {
+  try {
+    const response = await fetch(
+      "https://titin-back-production.up.railway.app/touch",
+      {
+        method: "GET",
+      }
+    );
+    console.log(response);
+    if (!response.ok) {
+      console.error("Error en la solicitud", response.status);
+    }
+  } catch (error) {
+    console.error("Error en la solicitud", error);
+  }
+};
+
+export { postCounter, getCounter };
