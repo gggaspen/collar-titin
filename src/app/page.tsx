98e9cba0d99afdbdee4b2a68070f3533b774877e
gggaspen <gggaspen@gmail.com>
2025-08-12 08:35:47 +0000

    jhvgyvjgh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/home/Home";
 import React, { useEffect } from "react";
 import { postCounter } from "./services/counter.service";
 import HomeV2 from "./pages/home/HomeV2";
+import HomeV3 from "./pages/home/HomeV3";
 
 function Page() {
   useEffect(() => {
@@ -11,7 +12,8 @@ function Page() {
   }, []);
 
   // return <Home />;
-  return <HomeV2 />;
+  // return <HomeV2 />;
+  return <HomeV3 />;
 }
 
 export default Page;
